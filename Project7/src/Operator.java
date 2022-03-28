590b76132bcd6ac70c4509dc90e2a4f9fe39ff2a
Lim-jeongho <limjh0387@gmail.com>
2022-03-28 12:01:32 +0900

    우선순위 연산자 예제

---
+++ Project7/src/Operator.java
@@ -0,0 +1,15 @@
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int z = ++x * y--;
+		System.out.printf("%d\t%d\t%d\n", x, y, z);
+		
+		int year = 2022;
+		System.out.println(year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+		
+	}
+
+}
