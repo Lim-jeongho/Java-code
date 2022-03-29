3187ec21dda80094459dbc0e0b618aa5ff8f236a
Lim-jeongho <101658853+Lim-jeongho@users.noreply.github.com>
2022-03-29 13:37:54 +0900

    Update Result.java
    double 타입 d1변수 선언 후 출력

--- Project9/src/Result.java
+++ Project9/src/Result.java
@@ -6,6 +6,8 @@ public class Result {
 		final int ONE = 1;
 		char c1 = 'a';
 		float f1 = 1.5f;
+		double d1 = 2.8;
+		
 		System.out.printf("i1 = %d\n", i1);
 		System.out.printf("i1/4 = %d\n", i1 / 4);
 		System.out.println("c1 + ONE = " + c1 + ONE);
@@ -14,6 +16,9 @@ public class Result {
 		System.out.printf("(c1 + ONE) = %c\n", c1 + ONE);
 		System.out.printf("(c1 + ONE) = %5s\n", c1 + ONE);
 		System.out.printf("i2 + f1 = %f\n", i2 + f1);
+		System.out.printf("f1 + d1 = %.1f\n", f1 + d1);
+		System.out.printf("(int)(f1 + d1) = %d\n", (int) (f1 + d1));
+		System.out.printf("(int)f1 + (int)d1 = %d\n", (int) f1 + (int) d1);
 	}
 
 }
