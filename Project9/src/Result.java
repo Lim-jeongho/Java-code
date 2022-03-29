7c4dcc358b6a6ac59be363afeb5f0e971eef3078
Lim-jeongho <limjh0387@gmail.com>
2022-03-29 12:14:53 +0900

    연습

---
+++ Project9/src/Result.java
@@ -0,0 +1,14 @@
+
+public class Result {
+
+	public static void main(String[] args) {
+		int i1 = 0x10, i2 = 5;
+		final int ONE = 1;
+		char c1 = '2';
+		System.out.printf("i1 = %d\n", i1);
+		System.out.printf("i1/4 = %d\n", i1 / 4);
+		System.out.println("c1 + ONE = " + c1 + ONE);
+
+	}
+
+}
