9d693d67a015f7b135acd7959cb103abd410cb53
Lim-jeongho <limjh0387@gmail.com>
2022-04-02 01:30:03 +0900

    switch문을 활용한 예제

---
+++ Project13/src/swich.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class swich {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("등수를 입력하세요 : ");
+		int rank = in.nextInt();
+		
+		switch (rank) {
+		case 1:
+			System.out.println("1등입니다");
+			break;
+		case 2:
+			System.out.println("2등입니다");
+			break;
+		case 3:
+			System.out.println("3등입니다");
+			break;
+		default:
+			System.out.println("등외입니다");
+		}
+	}
+
+}
