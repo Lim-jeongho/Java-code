6861ba819f315862af0c7ad52ea43f813fd98ff4
Lim-jeongho <101658853+Lim-jeongho@users.noreply.github.com>
2022-04-18 18:58:01 +0900

    for 문을 활용한 예제
    1000~10000합 추가

--- Project26/src/Method1.java
+++ Project26/src/Method1.java
@@ -4,17 +4,22 @@ public class Method1 {
 		int sum = 0;
 		for (int i = 0; i <= 10; i++)
 			sum += i;
-		System.out.println("ÇÕ(1~10) : " +sum);
+		System.out.println("Ã‡Ã•(1~10) : " +sum);
 		sum = 0;
 		
 		for (int i = 10; i <= 100; i++)
 			sum += i;
-		System.out.println("ÇÕ(10~100) : " +sum);
+		System.out.println("Ã‡Ã•(10~100) : " +sum);
 		sum = 0;
 		
 		for (int i = 100; i <= 1000; i++)
 			sum += i;
-		System.out.println("ÇÕ(100~1000) : " +sum);
+		System.out.println("Ã‡Ã•(100~1000) : " +sum);
+		sum = 0;
+		
+		for (int i = 1000; i <= 10000; i++)
+			sum += i;
+		System.out.println("Ã‡Ã•(1000~10000) : " +sum);
 	}
 
 }
