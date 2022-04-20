5e16aee013af930d9c2186ec71c034a356d1f24d
Lim-jeongho <limjh0387@gmail.com>
2022-04-21 01:30:55 +0900

    method 활용한 예제(매개변수)

---
+++ Project29/src/Echo.java
@@ -0,0 +1,12 @@
+public class Echo {
+
+	public static void main(String[] args) {
+		echo("¾È³ç!", 5);
+	}
+	
+	public static void echo(String s, int n) {
+		for (int i = 0; i < n; i++)
+			System.out.println(s);
+	}
+
+}
