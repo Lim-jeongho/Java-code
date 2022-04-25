f4ee5c3d10ccf8440e549dbb360869c87e3890b6
Lim-jeongho <limjh0387@gmail.com>
2022-04-25 23:08:13 +0900

    switch문을 활용한 예제

---
+++ Project32/src/switch0.java
@@ -0,0 +1,13 @@
+public class switch0 {
+
+	public static void main(String[] args) {
+		int a = 1, b = 2;
+		switch(a++) {
+		case 1: b+= 10;
+		case 2: b+= 5;
+		System.out.print(b);
+		}
+
+	}
+
+}
