d2010a9ec643ee05b6e09d281a7baafd380c9a59
Lim-jeongho <101658853+Lim-jeongho@users.noreply.github.com>
2022-05-13 20:47:46 +0900

    compareTo() 메서드 활용

--- Project48/src/String0.java
+++ Project48/src/String0.java
@@ -9,6 +9,9 @@ public class String0 {
 		System.out.println(s1.equals(s3));
 		System.out.println(s1.equals(s4));
 		System.out.println(s1.equalsIgnoreCase(s4));
+		
+		System.out.println(s1.compareTo(s3));  // 문자열을 사전 순으로 비교해 정수 값을 반환
+		System.out.println(s1.compareToIgnoreCase(s4));  // 대,소문자를 무시하고 문자열을 사전 순으로 비교
 		}
 
 }
