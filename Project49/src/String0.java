09735556f55ddae1b63c8a24077ef9baa64a9e01
Lim-jeongho <limjh0387@gmail.com>
2022-05-15 23:58:11 +0900

    String 메서드 활용

---
+++ Project49/src/String0.java
@@ -0,0 +1,9 @@
+public class String0 {
+	public static void main(String[] args) {
+		String s1 = new String("Hi");
+		
+		System.out.println("문자열 길이(s1) : " + s1.length());
+		char c = s1.charAt(1);
+		System.out.println(c);
+	}
+}
