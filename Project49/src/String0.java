9fb703fd5331ba07bb36060b58f422ae6ce1e8b5
Lim-jeongho <101658853+Lim-jeongho@users.noreply.github.com>
2022-05-16 23:43:16 +0900

    String() 메서드 이용
    string s2 추가

--- Project49/src/String0.java
+++ Project49/src/String0.java
@@ -1,9 +1,14 @@
 public class String0 {
 	public static void main(String[] args) {
 		String s1 = new String("Hi");
+		String s2 = new String("Java");
 		
-		System.out.println("¹®ÀÚ¿­ ±æÀÌ(s1) : " + s1.length());
+		System.out.println("Â¹Â®Ã€ÃšÂ¿Â­ Â±Ã¦Ã€ÃŒ(s1) : " + s1.length());
 		char c = s1.charAt(1);
 		System.out.println(c);
+		
+		s1 = s1.concat(s2);
+		
+		System.out.println(s1 + "!");
 	}
 }
