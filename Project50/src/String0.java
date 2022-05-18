b528a191e1f23f35d07c1e84b5bf63910cc4ba8e
Lim-jeongho <101658853+Lim-jeongho@users.noreply.github.com>
2022-05-18 21:42:59 +0900

    string 문자열 결합
    char 타입 java 'A'선언

--- Project50/src/String0.java
+++ Project50/src/String0.java
@@ -1,8 +1,13 @@
 public class String0 {
 	public static void main(String[] args) {
 		int i = 7;
+		char java = 'A';
 		System.out.println("Java " + i);
 		System.out.println("Java " + 7);
 		System.out.println(7 + 1 + "Java " + 7 + 1);
+		
+		System.out.println(java);
+		System.out.println(java + 7);
 	}
 }
+
