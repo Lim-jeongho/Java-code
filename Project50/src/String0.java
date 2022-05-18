e0cbf45839e749cddef78d2b200966934daf5fed
Lim-jeongho <limjh0387@gmail.com>
2022-05-18 20:53:41 +0900

    string 문자열 결합

---
+++ Project50/src/String0.java
@@ -0,0 +1,8 @@
+public class String0 {
+	public static void main(String[] args) {
+		int i = 7;
+		System.out.println("Java " + i);
+		System.out.println("Java " + 7);
+		System.out.println(7 + 1 + "Java " + 7 + 1);
+	}
+}
