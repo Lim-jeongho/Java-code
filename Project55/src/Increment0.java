7fedb1024de77cf9a4880f53677ce6863067f08d
Lim-jeongho <limjh0387@gmail.com>
2022-05-23 23:56:43 +0900

    메서드의 인수로 배열 전달

---
+++ Project55/src/Increment0.java
@@ -0,0 +1,16 @@
+public class Increment0 {
+
+	public static void main(String[] args) {
+		int[] x = { 0 };
+		System.out.println("호출 전의 x[0] = " + x[0]);
+		increment(x);
+		System.out.println("호출 후의 x[0] = " + x[0]);
+	}
+
+	public static void increment(int[] n) {
+		System.out.print("increment() 메서드 안에서 ");
+		System.out.print("n[0] = " + n[0] + " --> ");
+		n[0]++;
+		System.out.println("n[0] = " + n[0]);
+	}
+}
