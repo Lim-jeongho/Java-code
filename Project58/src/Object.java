0ad658159d440d7fa1f4039f064fa1bad7b6ce78
Lim-jeongho <limjh0387@gmail.com>
2022-05-27 17:15:32 +0900

    객체 인수와 기초타입 인수

---
+++ Project58/src/Object.java
@@ -0,0 +1,25 @@
+class Circle{
+	double radius;
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+}
+
+public class Object {
+	public static void main(String[] args) {
+		Circle c1 = new Circle(10.0);
+		Circle c2 = new Circle(10.0);
+		
+		zero(c1);
+		System.out.println("원(c1)의 반지름 : " + c1.radius);
+		
+		zero(c2.radius);
+		System.out.println("원(c2)의 반지름 : " + c2.radius);
+	}
+	public static void zero(Circle c) {
+		c.radius = 0.0;
+	}
+	public static void zero(double r) {
+		r = 0.0;
+	}
+}
