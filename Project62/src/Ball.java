509770f2bf25d7b81b214e7f521209956afa6b4a
Lim-jeongho <limjh0387@gmail.com>
2022-06-02 13:36:13 +0900

    Circle 클래스의 자식 클래스 Ball

---
+++ Project62/src/Ball.java
@@ -0,0 +1,12 @@
+public class Ball extends Circle{
+	private String color;
+	public Ball(String color) {
+		this.color = color;
+	}
+	public void findColor() {
+		System.out.println(color + " 공이다.");
+	}
+	public void findVolume() {
+		System.out.println("부피는 4/3*(π*반지름*반지름*반지름)이다.");
+	}
+}
