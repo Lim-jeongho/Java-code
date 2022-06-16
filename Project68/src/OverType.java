4d734f58b9d1e4a332d46c40d7efa9501a4f07c5
Lim-jeongho <limjh0387@gmail.com>
2022-06-16 16:35:08 +0900

    오버라이딩 여부에 따른 타입 변환

---
+++ Project68/src/OverType.java
@@ -0,0 +1,26 @@
+class Vehicle {
+	String name = "탈 것";
+	void whoami() {
+		System.out.println("나는 탈 것이다.");
+		}
+	static void move() {
+		System.out.println("이동하다.");
+	}
+}
+class Car extends Vehicle {
+	String name = "자동차";
+	void whoami() {
+		System.out.println("나는 자동차이다.");
+	}
+	static void move() {
+		System.out.println("달리다.");
+	}
+}
+public class OverType {
+	public static void main(String[] args) {
+		Vehicle v = new Car();
+		System.out.println(v.name);
+		v.whoami();
+		v.move();
+	}
+}
