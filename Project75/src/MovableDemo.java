832d772709b5564ce3a468b6474399d243a70788
Lim-jeongho <limjh0387@gmail.com>
2022-06-29 11:23:16 +0900

    인터페이스와 다형성2

---
+++ Project75/src/MovableDemo.java
@@ -0,0 +1,23 @@
+interface Movable {
+	void move(int x);
+}
+class Car implements Movable {
+	private int pos = 0;
+	public void move(int x) {
+		pos += x;
+	}
+	public void show() {
+		System.out.println(pos + "m 이동했습니다.");
+	}
+}
+
+public class MovableDemo {
+	public static void main(String[] args) {
+		Movable m = new Car();
+		m.move(5);;
+		Car c = (Car) m;
+		c.move(10);
+		c.show();
+	}
+
+}
