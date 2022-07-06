6006430fb91a1315524706424a8c014fb82cbc4a
Lim-jeongho <limjh0387@gmail.com>
2022-07-06 12:43:20 +0900

    비교할 수 있는 객체의 정렬

---
+++ Project80/src/ComparableDemo.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Rectangle implements Comparable {
+	private int width, height;
+	
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	public int findArea() {
+		return width * height;
+	}
+	public String toString() {
+		return "사각형[넓이=" + width + ", 높이=" + height + "]";
+	}
+	
+	public int compareTo(Object o) {
+		Rectangle other = (Rectangle) o;
+		
+		if (this.findArea() < other.findArea())
+			return -1;
+		else if (this.findArea() > other.findArea())
+			return 1;
+		else
+			return 0;
+	}
+}
+public class ComparableDemo {
+	public static void main(String[] args) {
+		Rectangle[] rectangles = {new Rectangle(3, 5),
+				new Rectangle(2, 10), new Rectangle(5, 5)};
+		Arrays.sort(rectangles);
+		for (Rectangle r : rectangles)
+			System.out.println(r);
+	}
+}
