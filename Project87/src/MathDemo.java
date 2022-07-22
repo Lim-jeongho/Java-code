06d2432a8a736893c40bbf7663b79cb111ce5896
Lim-jeongho <101658853+Lim-jeongho@users.noreply.github.com>
2022-07-22 14:11:39 +0900

    Math 클래스의 활용

--- Project87/src/MathDemo.java
+++ Project87/src/MathDemo.java
@@ -3,7 +3,7 @@ public class MathDemo {
 		System.out.println("Math.pow(2, 8) : " + Math.pow(2, 8));
 		System.out.println("Math.random() : " + Math.random());
 		System.out.println("Math.sin(Math.PI) : " + Math.sin(Math.PI));
-		System.out.println("Math.min(10, 20) : " + Math.min(10, 20));
+		System.out.println("Math.min(10, 20) : " + Math.min(10, 20)); 
 		
 	}
 
