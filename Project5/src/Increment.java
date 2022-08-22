1f44cd6a35255dbdb1f4cad884fad73706bbe683
Lim-jeongho <101658853+Lim-jeongho@users.noreply.github.com>
2022-08-23 01:05:54 +0900

    증감 연산자 활용한 예제

--- Project5/src/Increment.java
+++ Project5/src/Increment.java
@@ -4,8 +4,8 @@ public class Increment {
 	public static void main(String[] args) {
 		int plusOne = 1;
 		int minusOne = -plusOne;
-		System.out.println("plusOne�� " + plusOne + "�Դϴ�.");
-		System.out.println("minusOne�� " + minusOne + "�Դϴ�.");
+		System.out.println("plusOneÀº " + plusOne + "ÀÔ´Ï´Ù.");
+		System.out.println("minusOneÀº " + minusOne + "ÀÔ´Ï´Ù.");
 		
 		int x = 1;
 		int y = 1;
@@ -14,4 +14,4 @@ public class Increment {
 		System.out.println("x = " + x + ", y = " + y);
 	}
 
-}
+} 
