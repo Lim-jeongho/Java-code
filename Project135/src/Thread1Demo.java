af26e9bbdd0e2bc9292decf91a4e8ccc4c582379
Lim-jeongho <limjh0387@gmail.com>
2022-09-04 22:55:47 +0900

    Runnable 구현 클래스를 이용한 스레드

---
+++ Project135/src/Thread1Demo.java
@@ -0,0 +1,25 @@
+public class Thread1Demo {
+	public static void main(String[] args) {
+		Thread t = new Thread(new MyRunnable());
+		t.start();
+		for (int i = 0; i < 5; i++) {
+			System.out.print("¾È³ç. ");
+			try {
+				Thread.sleep(500);
+			} catch(InterruptedException e) {
+			}
+		}
+	}
+}
+
+class MyRunnable implements Runnable {
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Àß°¡. ");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
