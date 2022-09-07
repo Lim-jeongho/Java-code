9f8d695539e4c02c1dd5d5a98e9ebc8ed6acb975
Lim-jeongho <limjh0387@gmail.com>
2022-09-07 15:11:11 +0900

    조건을 이용한 스레드 종료

---
+++ Project137/src/StopDemo.java
@@ -0,0 +1,27 @@
+class StopThread extends Thread {
+	public boolean stop;
+	public void run() {
+		while (!stop) {
+			System.out.println("실행 중...");
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				
+			}
+		}
+		System.out.println("종료");
+	}
+}
+
+public class StopDemo {
+	public static void main(String[] args) {
+		StopThread t = new StopThread();
+		t.start();
+		try {
+			Thread.sleep(3);
+		} catch (InterruptedException e) {
+			
+		}
+		t.stop = true;
+	}
+}
