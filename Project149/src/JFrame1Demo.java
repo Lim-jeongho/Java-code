bdf145745244c0b1c62bfcea5dcb07897e0066b9
Lim-jeongho <limjh0387@gmail.com>
2022-09-21 14:11:01 +0900

    JFrame의 자식 클래스

---
+++ Project149/src/JFrame1Demo.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+class MyFrame extends JFrame {
+	MyFrame() {
+		setTitle("¾È³ç, ½ºÀ®");
+		setSize(300, 100);
+		setVisible(true);
+	}
+}
+
+public class JFrame1Demo {
+	public static void main(String[] args) {
+		new MyFrame();
+	}
+}
