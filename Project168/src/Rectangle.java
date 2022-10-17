fbc7061f3f4c281054c0cd2f122ac7bc6bffcf14
Lim-jeongho <limjh0387@gmail.com>
2022-10-18 05:59:01 +0900

    다양한 사각형 그리기

---
+++ Project168/src/Rectangle.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Rectangle extends JFrame {
+	Rectangle() {
+		setTitle("다양한 사각형 그리기");
+		add(new JPanel() {
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.setColor(Color.RED);
+				g.drawRect(30, 10, 50, 50);
+				g.drawRoundRect(120, 10, 50, 50, 30, 20);
+				g.draw3DRect(210, 10, 50, 50, false);
+				g.draw3DRect(300, 10, 50, 50, true);
+				g.setColor(Color.GREEN);
+				g.drawRect(30, 80, 50, 50);
+				g.drawRoundRect(120, 80, 50, 50, 30, 20);
+				g.draw3DRect(210, 80, 50, 50, false);
+				g.draw3DRect(300, 80, 50, 50, true);
+			}
+		});
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 180);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Rectangle();
+	}
+}
