801217b96e0a4a524445bb44267cdfce3422ea4b
Lim-jeongho <limjh0387@gmail.com>
2022-11-26 23:59:06 +0900

    Collections 클래스의 활용

---
+++ Project204/src/CollectionsDemo.java
@@ -0,0 +1,34 @@
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class CollectionsDemo {
+	static void printList(LinkedList<String> 1) {
+		Iterator<String> iterator = l.iterator();
+		while(iterator.hasNext()) {
+			String e = iterator.next();
+			String separator;
+			if(iterator.hasNext())
+				separator = "->";
+			else
+				separator = "\n";
+			System.out.print(e+separator);
+		}
+	}
+	
+	public static void main(String[] args) {
+		LinkedList<String> myList = new LinkedList<String>();
+		myList.add("트랜스포머");
+		myList.add("스타워즈");
+		myList.add("매트릭스");
+		myList.add(0, "터미네이터");
+		myList.add(2, "아바타");
+		Collections.sort(myList);
+		printList(myList);
+		Collections.reverse(myList);
+		printList(myList);
+		int index = Collections.binarySearch(myList, "아바타") + 1;
+		System.out.println("아바타는 " + index + "번째 요소입니다.");
+	}
+}
