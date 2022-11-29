7e81d1c9fb8d872bcd9fd269c005f481f8074827
Lim-jeongho <limjh0387@gmail.com>
2022-11-29 20:44:17 +0900

    독립 클래스로 Action 이벤트 리스너 작성

---
+++ Project207/src/IndepClassListener.java
@@ -0,0 +1,35 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class IndepClassListener extends JFrame {
+	public IndepClassListener() {
+		setTitle("Action 이벤트 리스너 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+		btn.addActionListener(new MyActionListener());
+		c.add(btn);
+		setSize(350, 150);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new IndepClassListener();
+	}
+}
+
+class MyActionListener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		JButton b = (JButton)e.getSource();
+		if(b.getText().equals("Action"))
+			b.setText("액션");
+		else
+			b.setText("Action");
+	}
+}
