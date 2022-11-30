d715ed2291a30ec1248ec30dd54a0202bce1f737
Lim-jeongho <limjh0387@gmail.com>
2022-11-30 18:46:02 +0900

    내부 클래스로 Action 이벤트 리스너 만들기

---
+++ Project208/src/InnerClassListener.java
@@ -0,0 +1,34 @@
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class InnerClassListener extends JFrame {
+	public InnerClassListener() {
+		setTitle("Action 이벤트 리스너 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+		btn.addActionListener(new MyActionListener());
+		c.add(btn);
+		setSize(350, 150);
+		setVisible(true);
+	}
+	private class MyActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JButton b = (JButton)e.getSource();
+			if(b.getText().equals("Action"))
+				b.setText("액션");
+			else
+				b.setText("Action");
+			InnerClassListener.this.setTitle(b.getText());
+		}
+	}
+	public static void main(String[] args) {
+		new InnerClassListener();
+	}
+}
