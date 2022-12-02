f557d153ecdff3b6a427c89c8936a5c63ac6f2db
Lim-jeongho <limjh0387@gmail.com>
2022-12-02 22:18:28 +0900

    마우스로 문자열 이동시키기

---
+++ Project210/src/MouseListenerDemo.java
@@ -0,0 +1,37 @@
+import java.awt.Container;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MouseListenerDemo extends JFrame {
+	private JLabel la = new JLabel("Hello");
+	public MouseListenerDemo() {
+		setTitle("Mouse 이벤트 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.addMouseListener((MouseListener) new MyMouseListener());
+		c.setLayout(null);
+		la.setSize(50, 20);
+		la.setLocation(30, 30);
+		c.add(la);
+		setSize(250, 250);
+		setVisible(true);
+	}
+	class MyMouseListener implements MouseListener {
+		public void mousePressed(MouseEvent e) {
+			int x = e.getX();
+			int y = e.getY();
+			la.setLocation(x, y);
+		}
+		public void mouseReleased(MouseEvent e) {}
+		public void mouseClicked(MouseEvent e) {}
+		public void mouseEntered(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+	}
+	
+	public static void main(String[] args) {
+		new MouseListenerDemo();
+	}
+}
