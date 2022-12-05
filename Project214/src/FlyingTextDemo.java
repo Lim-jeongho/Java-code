7bd57b948d28e458ff885762e435fd32fd8239c2
Lim-jeongho <limjh0387@gmail.com>
2022-12-06 03:13:59 +0900

    방향키로 문자열 움직이기

---
+++ Project214/src/FlyingTextDemo.java
@@ -0,0 +1,58 @@
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class FlyingTextDemo extends JFrame {
+	private final int FLYING_UNIT = 20;  // 레이블이 한 번 움직이는 단위 : 10픽셀
+	private JLabel la = new JLabel("HELLO");
+	public FlyingTextDemo() {
+		setTitle("상,하,좌,우 키를 이용하여 텍스트 움직이기");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(null);
+		c.addKeyListener((KeyListener) new MyKeyListener());
+		la.setLocation(50,50);  // 초기위치
+		la.setSize(100,20);
+		c.add(la);
+		setSize(300,300);
+		setVisible(true);
+		c.setFocusable(true);
+		c.requestFocus();
+		c.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				Component com = (Component)e.getSource();
+				com.setFocusable(true);
+				com.requestFocus();
+			}
+		});
+	}
+	class MyKeyListener extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			int keyCode = e.getKeyCode();
+			switch(keyCode) {
+			case KeyEvent.VK_UP:
+				la.setLocation(la.getX(), la.getY()-FLYING_UNIT);
+				break;
+			case KeyEvent.VK_DOWN:
+				la.setLocation(la.getX(), la.getY()+FLYING_UNIT);
+				break;
+			case KeyEvent.VK_LEFT:
+				la.setLocation(la.getX()-FLYING_UNIT, la.getY());
+				break;
+			case KeyEvent.VK_RIGHT:
+				la.setLocation(la.getX()+FLYING_UNIT, la.getY());
+				break;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		new FlyingTextDemo();
+	}
+}
