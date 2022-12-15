76b0be523521a1424e16b881169d703c23fb1cd9
Lim-jeongho <limjh0387@gmail.com>
2022-12-15 20:32:09 +0900

    JTextArea 컴포넌트 생성

---
+++ Project223/src/TextAreaEx.java
@@ -0,0 +1,36 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class TextAreaEx extends JFrame {
+	private JTextField tf = new JTextField(20);
+	private JTextArea ta = new JTextArea(7, 20);
+	public TextAreaEx() {
+		setTitle("텍스트영역 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		c.add(new JLabel("입력 후 <Enter> 키를 입력하세요"));
+		c.add(tf);
+		c.add(new JScrollPane(ta));
+		tf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JTextField t = (JTextField)e.getSource();
+				ta.append(t.getText() + "\n");
+				t.setText("");
+			}
+		});
+		setSize(300,300);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new TextAreaEx();
+	}
+}
