64c9d90907a2e2ad124670f4cd69b8b767d9ffe0
Lim-jeongho <limjh0387@gmail.com>
2022-12-17 23:09:30 +0900

    리스트의 아이템 변경

---
+++ Project225/src/ListChangeEx.java
@@ -0,0 +1,45 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+public class ListChangeEx extends JFrame {
+	private JTextField tf = new JTextField(10);
+	private Vector<String> v = new Vector<String>();
+	private JList<String> nameList = new JList<String>(v);
+	
+	public ListChangeEx() {
+		setTitle("리스트 변경 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		c.add(new JLabel("이름 입력 후 <Enter> 키"));
+		c.add(tf);
+		v.add("임정호");
+		nameList.setVisibleRowCount(5);
+		nameList.setFixedCellWidth(100);
+		c.add(new JScrollPane(nameList));
+		
+		setSize(300,300);
+		setVisible(true);
+		
+		tf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JTextField t = (JTextField)e.getSource();
+				v.add(t.getText());
+				nameList.setListData(v);
+			}
+		});
+	}
+	
+ 	public static void main(String[] args) {
+ 		new ListChangeEx();
+	}
+}
