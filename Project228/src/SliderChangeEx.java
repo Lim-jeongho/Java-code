3e08156de156e45b367d090239bd5d158d54efa2
Lim-jeongho <limjh0387@gmail.com>
2022-12-20 17:52:17 +0900

    JSlider와 Change이벤트 활용

---
+++ Project228/src/SliderChangeEx.java
@@ -0,0 +1,58 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class SliderChangeEx extends JFrame {
+	private JLabel colorLabel;
+	private JSlider [] sl = new JSlider [3];
+	
+	public SliderChangeEx() {
+		setTitle("슬라이더와 ChangeEvent 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		colorLabel = new JLabel(" 		SLIDER EXAMPLE		");
+		
+		for(int i=0; i<sl.length; i++) {
+			sl[i] = new JSlider(JSlider.HORIZONTAL, 0, 255, 128);
+			sl[i].setPaintLabels(true);
+			sl[i].setPaintTicks(true);
+			sl[i].setPaintTrack(true);
+			sl[i].setMajorTickSpacing(50);
+			sl[i].setMinorTickSpacing(10);
+			sl[i].addChangeListener(new MyChangeListener());
+			c.add(sl[i]);
+		}
+		
+		sl[0].setForeground(Color.RED);
+		sl[1].setForeground(Color.GREEN);
+		sl[2].setForeground(Color.BLUE);
+		int r = sl[0].getValue();
+		int g = sl[1].getValue();
+		int b = sl[2].getValue();
+		colorLabel.setOpaque(true);
+		colorLabel.setBackground(new Color(r,g,b));
+		c.add(colorLabel);
+		setSize(300,230);
+		setVisible(true);
+	}
+	
+	class MyChangeListener implements ChangeListener {
+		public void stateChanged(ChangeEvent e) {
+			int r = sl[0].getValue();
+			int g = sl[1].getValue();
+			int b = sl[2].getValue();
+			colorLabel.setBackground(new Color(r,g,b));
+		}
+	}
+	
+	public static void main(String[] args) {
+		new SliderChangeEx();
+	}
+}
