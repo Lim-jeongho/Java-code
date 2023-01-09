8b7991986b9b7315b389b271ddcf820f8f5754bd
Lim-jeongho <limjh0387@gmail.com>
2023-01-09 18:08:28 +0900

    일정 범위 안에서 최솟값 찾기

---
+++ Project247/src/MinEx.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import org.w3c.dom.Node;
+
+public class MinEx {
+	public static final Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		Deque<Node> mydeque = new LinkedList<>();
+		for(int i=0; i<N; i++) {
+			int now = Integer.parseInt(st.nextToken());
+			while(!mydeque.isEmpty() && mydeque.getLast().value > now) {
+				mydeque.removeLast();
+			}
+			mydeque.addLast(new Node(now, i));
+			if(mydeque.getFirst().index <= i - L) {
+				mydeque.removeFirst();
+			}
+			bw.write(mydeque.getFirst().value + " ");
+		}
+		bw.flush();
+		bw.close();
+	}
+	static class Node {
+		public int value;
+		public int index;
+		
+		Node(int value, int index) {
+			this.value = value;
+			this.index = index;
+		}
+	}
+}
