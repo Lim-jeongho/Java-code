362d89532fa46e3fa1f5073d84ff667c56f1d607
Lim-jeongho <limjh0387@gmail.com>
2023-03-05 14:01:31 +0900

    원하는 정수 찾기

---
+++ Project266/src/IntegerEx.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class IntegerEx {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		for(int i=0; i<N; i++) {
+			A[i] = sc.nextInt();
+		}
+		Arrays.sort(A);
+		int M = sc.nextInt();
+		for(int i=0; i<M; i++) {
+			boolean find = false;
+			int target = sc.nextInt();
+			int start = 0;
+			int end = A.length - 1;
+			while(start <= end) {
+				int midi = (start + end) / 2;
+				int midV = A[midi];
+				if(midV > target) {
+					end = midi - 1;
+				} else if (midV < target) {
+					start = midi + 1;
+				} else {
+					find = true;
+					break;
+				}
+			}
+			if (find) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
+		}
+	}
+}
