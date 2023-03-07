942f5d432827c2b034528824089b92571d5c58b6
Lim-jeongho <limjh0387@gmail.com>
2023-03-07 11:45:57 +0900

    배열에서 K번째 수 찾기

---
+++ Project268/src/FindnumberEx.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FindnumberEx {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		long start = 1, end = K;
+		long ans = 0;
+		while(start <= end) {
+			long middle = (start + end) / 2;
+			long cnt = 0;
+			for(int i=1; i<=N; i++) {
+				cnt += Math.min(middle / i, N);
+			}
+			if(cnt < K) {
+				start = middle + 1;
+			} else {
+				ans = middle;
+				end = middle - 1;
+			}
+		}
+		System.out.println(ans);
+	}
+}
