3946d8c3ebb4baeb41c4a6c9b249031be096e878
Lim-jeongho <limjh0387@gmail.com>
2023-03-17 22:28:07 +0900

    최소 공배수 구하기

---
+++ Project279/src/MinimunEx.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MinimunEx {
+	public static void main(String[] args) throws Exception {
+		Scanner sc= new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0; i<t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int result = a * b / gcd(a, b);
+			System.out.println(result);
+		}
+	}
+	public static int gcd(int a, int b) {
+		if(b == 0)
+			return a;
+		else
+			return gcd(b, a % b);
+	}
+}
