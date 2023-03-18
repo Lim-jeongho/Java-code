2b1515d8003a5c713294e4b086dc673266f4dedb
Lim-jeongho <limjh0387@gmail.com>
2023-03-18 19:13:30 +0900

    최대 공약수 구하기

---
+++ Project280/src/MaximumEx.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class MaximumEx {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long result = gcd(a, b);
+		while(result > 0) {
+			bw.write("1");
+			result--;
+		}
+		bw.flush();
+		bw.close();
+	}
+	public static long gcd(long a, long b) {
+		if(b == 0)
+			return a;
+		else
+			return gcd(b, a % b);
+	}
+}
